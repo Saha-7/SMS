ad419a9cfe34d33f6cfd5a9b854557e1682abae9
saha-7op <saha7pritam@gmail.com>
2026-02-05 13:53:06 +0530

    Updated Student Entity (with validation)

--- src/main/java/net/javaguides/sms/entity/Student.java
+++ src/main/java/net/javaguides/sms/entity/Student.java
@@ -1,11 +1,8 @@
 package net.javaguides.sms.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "students")
@@ -16,12 +13,16 @@ public class Student {
 	private Long id;
 	
 	@Column(name = "first_name", nullable = false)
+	@NotBlank(message = "First name is required")
 	private String firstName;
 	
 	@Column(name = "last_name")
+	@NotBlank(message = "Last name is required")
 	private String lastName;
 	
 	@Column(name = "email")
+	@NotBlank(message = "Email is required")
+	@Email(message = "Please provide a valid email address")
 	private String email;
 	
 	public Student() {
@@ -34,27 +35,35 @@ public class Student {
 		this.lastName = lastName;
 		this.email = email;
 	}
+	
 	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
 	public String getFirstName() {
 		return firstName;
 	}
+	
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
 	public String getLastName() {
 		return lastName;
 	}
+	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
